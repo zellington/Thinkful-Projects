658e0d36bc26ba6570c4cc5e70c6a144f0eaa55c
zellington <zacellington@gmail.com>
2015-01-30 14:55:31 -0500

    Update snippets app

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -1,18 +1,24 @@
 import logging
+import csv
+import argparse
+import sys
 
 # Set the log output file, and the log level
-logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
-def put(name, snippet):
-    """
-    Store a snippet with an associated name.
-
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+def put(name, snippet, filename):
+    """ Store a snippet with an associated name in the CSV file """
+    logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
+    logging.debug("Opening file")
+    with open(filename, "a") as f:
+        writer = csv.writer(f)
+        logging.debug("Writing snippet to file")
+        writer.writerow([name, snippet])
+    logging.debug("Write sucessful")
     return name, snippet
 
- def get(name):
+
+def get(name):
     """Retrieve the snippet with a given name.
 
     If there is no such snippet...
@@ -22,3 +28,35 @@ def put(name, snippet):
     #return and fill in '...'
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+
+def make_parser():
+    """Construct the comman line parser """
+    logging.info("Construting parser")
+    description = "Store and retieve snippets of text"
+    parser = argparse.ArgumentParser(description=description)
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    #Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    put_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
+    
+    return parser
+
+def main():
+    """ Main function """
+    logging.info("Starting snippets")
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print "Stored {!r} as {!r}".format(snippet, name)
+
+if __name__ == "__main__":
+    main()
