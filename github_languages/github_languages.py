94a14a1962a13035ccfe1d855a247f7ca6e89238
zellington <zacellington@gmail.com>
2015-03-08 11:59:49 -0400

    Updates

--- github_languages/github_languages.py
+++ github_languages/github_languages.py
@@ -10,10 +10,43 @@ def get_repositories(user):
     response = requests.get(url, auth=(USERNAME, PASSWORD))
     return response.json()
 
+def get_language_dictionaries(repositories):
+    """
+    Return a list of dictionaries containing the languages used in each
+    repository
+    """
+    language_dictionaries = []
+    for repository in repositories:
+        url = "https://api.github.com/repos/{owner}/{repo}/languages"
+        url = url.format(owner=repository["owner"]["login"],
+                         repo=repository["name"])
+        response = requests.get(url, auth=(USERNAME, PASSWORD))
+        language_dictionaries.append(response.json())
+    return language_dictionaries
+    
+def accumulate_languages(language_dictionaries):
+    """ Calculate the total data size for each language """
+    accumulated = defaultdict(int)
+    total = 0
+    for language_dictionary in language_dictionaries:
+        for language_name, number_of_bytes in language_dictionary.iteritems():
+            accumulated[language_name] += number_of_bytes
+            total += number_of_bytes
+    return accumulated, total
+
 def main():
     """ Main function """
     repositories = get_repositories(sys.argv[1])
-    print repositories
+    language_dictionaries = get_language_dictionaries(repositories)
+    language_totals, total_bytes = accumulate_languages(language_dictionaries)
+
+    sorted_language_totals = sorted(language_totals.iteritems(),
+                                    key=operator.itemgetter(1),
+                                    reverse=True)
+
+    for language_name, number_of_bytes in sorted_language_totals:
+        percentage = 100.0 * number_of_bytes / total_bytes
+        print "{}: {:.2f}%".format(language_name, percentage)
     
 
 if __name__ == "__main__":
