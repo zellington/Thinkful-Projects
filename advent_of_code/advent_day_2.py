283b5327b7f1809eaefb522c4f8d60a5419f1095
zellington <zacellington@gmail.com>
2015-12-20 23:19:41 -0500

    adds advent of code

---
+++ advent_of_code/advent_day_2.py
@@ -0,0 +1,24 @@
+#from day_2_data import data 
+
+
+
+test = '''20x31x1x4'''
+new_list = []
+new_list.append(test)
+#l,w,h=(int(dim) for dim in test.split("x"))
+
+#print l+w+h
+print new_list
+for x in range(3):
+	print new_list[:]
+#print min(sides)
+
+#find a way to order the 3 and find the smallest
+
+#add smallest
+
+
+
+#fruits = ['banana', 'apple',  'mango']
+#for index in range(len(fruits)):
+#  print 'Current fruit :', fruits[index]
