9641d85b91fcf26ac98c957c217017d2f850bc8d
zellington <zacellington@gmail.com>
2015-12-22 12:01:31 -0500

    Solves Day 2 Challenge

--- advent_of_code/advent_day_2.py
+++ advent_of_code/advent_day_2.py
@@ -1,24 +1,40 @@
-#from day_2_data import data 
+
+from day_2_data import data 
 
 
+# total = 0	
 
-test = '''20x31x1x4'''
-new_list = []
-new_list.append(test)
-#l,w,h=(int(dim) for dim in test.split("x"))
+# for row in data.split('\n'):
+# 	l,w,h = [int(x) for x in row.split('x')]
+	
+# 	total += ((2*l*w + 2*w*h + 2*h*l)+min((l*w), (w*h), (h*l)))
 
-#print l+w+h
-print new_list
-for x in range(3):
-	print new_list[:]
-#print min(sides)
-
-#find a way to order the 3 and find the smallest
-
-#add smallest
+# print "the total is:" , total
 
 
+# --DAY 2 Part 2 Solution
 
-#fruits = ['banana', 'apple',  'mango']
-#for index in range(len(fruits)):
-#  print 'Current fruit :', fruits[index]
+
+test = '''1x1x10
+20x3x11
+15x27x5
+6x29x7'''
+# 15x27x5
+# 6x29x7'''
+
+count_feet = 0	
+
+for row in data.split('\n'):
+	l,w,h = [int(x) for x in row.split('x')]
+	bow = (l*w*h)
+	print "the bow is: ", bow
+	ribbon = (2*(min((l+w),(w+h),(l+h))))
+	#ribbon = ribbon_list.append(l)
+	print "the ribbon is: " , ribbon
+	count_feet = (count_feet+ribbon+bow)
+	print "the count is:", count_feet
+# 	total_ribbon = bow+ribbon
+# 	print "the total_ribbon is: " , total_ribbon
+# 	count_feet = count_feet + total_ribbon
+print "the final total is: ", count_feet
+
